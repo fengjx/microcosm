c7b6f312de80f00396ef821030612992985a6952
FengJianxin <fengjianxin2012@gmail.com>
2019-05-29 00:40:25 +0800

    update | move to service

--- internal/routers/api/TestApi.go
+++ internal/routers/api/TestApi.go
@@ -2,12 +2,14 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"microcosm/internal/module/test"
+	"microcosm/internal/service"
 	"net/http"
 )
 
+var testService = service.GetTestService()
+
 func TestList(c *gin.Context) {
-	list, err := test.FindList()
+	list, err := testService.FindList()
 	if err != nil {
 		c.String(http.StatusOK, err.Error())
 	}
