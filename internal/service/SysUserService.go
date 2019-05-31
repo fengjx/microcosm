54f0a8e4cf94dc6add20efee052d108243f1e967
gzfengjianxin <gzfengjianxin@corp.netease.com>
2019-05-31 18:24:36 +0800

    add | 增加查询删除方法

--- internal/service/SysUserService.go
+++ internal/service/SysUserService.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/jinzhu/gorm"
 	"microcosm/internal/db"
 	"microcosm/internal/model"
 	"microcosm/internal/pkg/utils"
@@ -12,9 +13,11 @@ var sysUserService *SysUserService
 
 func init() {
 	sysUserService = new(SysUserService)
+	sysUserService.orm = db.GetDB()
 }
 
 type SysUserService struct {
+	orm *gorm.DB
 }
 
 func (receiver *SysUserService) AddUser(data *form.AddSysUserForm) error {
@@ -29,10 +32,30 @@ func (receiver *SysUserService) AddUser(data *form.AddSysUserForm) error {
 	return receiver.save(sysUser)
 }
 
+func (receiver SysUserService) UpdateSysUser(sysUser *model.SysUser) error {
+	return receiver.orm.Save(sysUser).Error
+}
+
 func (receiver *SysUserService) save(sysUser *model.SysUser) error {
-	orm := db.GetDB()
-	orm.Create(&sysUser)
-	return nil
+	return receiver.orm.Create(&sysUser).Error
+}
+
+func (receiver SysUserService) DeleteSysUser(id uint32) error {
+	user := new(model.SysUser)
+	user.Id = id
+	return receiver.orm.Delete(user).Error
+}
+
+func (receiver *SysUserService) GetSysUserById(id uint32) model.SysUser {
+	var user model.SysUser
+	receiver.orm.First(&user, id)
+	return user
+}
+
+func (receiver SysUserService) FindSysUserList(offset int8, limit uint, orderBy string) error {
+	var user model.SysUser
+	receiver.orm.Offset(offset).Limit(limit).Order(orderBy).Find(&user).
+
 }
 
 func GetSysUserService() *SysUserService {
